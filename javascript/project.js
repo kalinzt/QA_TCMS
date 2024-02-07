3874bdbede3c3117e4861a06745d1af3f4493a3c
kalinzt <kalinzt@gmail.com>
2024-02-07 16:35:48 +0900

    _sub

--- javascript/project.js
+++ javascript/project.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function active_subSection() {
     // 초기에 첫 번째 섹션을 활성화
-    activateSection("allProject");
+    activate_subSection("allProject");
 
     // 각 메뉴를 클릭했을 때 해당 섹션을 활성화
     document.querySelectorAll("li a").forEach(function (menuLink) {
@@ -9,12 +9,12 @@ document.addEventListener("DOMContentLoaded", function active_subSection() {
 
             // 클릭한 메뉴에 해당하는 섹션 활성화
             var targetSectionId = menuLink.getAttribute("data-target");
-            activateSection(targetSectionId);
+            activate_subSection(targetSectionId);
         });
     });
 
     // 섹션을 활성화하는 함수
-    function activateSection(sectionId) {
+    function activate_subSection(sectionId) {
         // 모든 섹션 비활성화
         document.querySelectorAll("section").forEach(function (section) {
             section.classList.remove("active");
@@ -24,16 +24,16 @@ document.addEventListener("DOMContentLoaded", function active_subSection() {
         document.getElementById(sectionId).classList.add("active");
 
         // 해당 섹션에 HTML 파일 불러오기
-        fetchSectionContent(sectionId);
+        fetch_subSectionContent(sectionId);
     }
 
     // 섹션에 HTML 파일을 불러오는 함수
-    function fetchSectionContent(sectionId) {
+    function fetch_subSectionContent(sectionId) {
         var sectionContainer = document.getElementById(sectionId);
-        var url = "/HTML/" + sectionId + ".html"; // HTML 파일 경로
+        var url = sectionId + ".html"; // HTML 파일 경로
 
 
-        fetch(url)
+        fetch_sub(url)
             .then(function (response) {
                 return response.text();
             })
